06dd105d987b0ff700beca7a8eb88a82dfc97955
timk <timurlan77@gmail.com>
2020-08-09 12:30:32 -0400

    more tweaks

--- src/demux/demuxer-inline.js
+++ src/demux/demuxer-inline.js
@@ -16,18 +16,24 @@ import MP4Remuxer from '../remux/mp4-remuxer';
 import PassThroughRemuxer from '../remux/passthrough-remuxer';
 
 import { getSelfScope } from '../utils/get-self-scope';
-import { logger } from '../utils/logger';
+import { logger }       from '../utils/logger';
 
 // see https://stackoverflow.com/a/11237259/589493
 const global = getSelfScope(); // safeguard for code that might run both on worker and main thread
 
-let now;
 // performance.now() not available on WebWorker, at least on Safari Desktop
+let now;
 try {
-  now = global.performance.now.bind(global.performance);
+  if (global && global.performance && global.performance.now) {
+    now = global.performance.now.bind(global.performance);
+  } else {
+    now = global.Date.now;
+  }
 } catch (err) {
   logger.debug('Unable to use Performance API on this environment');
-  now = global.Date.now;
+  if (Date) {
+    now = Date.now;
+  }
 }
 
 class DemuxerInline {
@@ -55,7 +61,12 @@ class DemuxerInline {
       const startTime = now();
       decrypter.decrypt(data, decryptdata.key.buffer, decryptdata.iv.buffer, (decryptedData) => {
         const endTime = now();
-        this.observer.trigger(Event.FRAG_DECRYPTED, { stats: { tstart: startTime, tdecrypt: endTime } });
+        this.observer.trigger(Event.FRAG_DECRYPTED, {
+          stats: {
+            tstart: startTime,
+            tdecrypt: endTime
+          }
+        });
         this.pushDecrypted(new Uint8Array(decryptedData), decryptdata, new Uint8Array(initSegment), audioCodec, videoCodec, timeOffset, discontinuity, trackSwitch, contiguous, duration, accurateTimeOffset, defaultInitPTS);
       });
     } else {
@@ -75,10 +86,22 @@ class DemuxerInline {
       const config = this.config;
       // probing order is TS/MP4/AAC/MP3
       const muxConfig = [
-        { demux: TSDemuxer, remux: MP4Remuxer },
-        { demux: MP4Demuxer, remux: PassThroughRemuxer },
-        { demux: AACDemuxer, remux: MP4Remuxer },
-        { demux: MP3Demuxer, remux: MP4Remuxer }
+        {
+          demux: TSDemuxer,
+          remux: MP4Remuxer
+        },
+        {
+          demux: MP4Demuxer,
+          remux: PassThroughRemuxer
+        },
+        {
+          demux: AACDemuxer,
+          remux: MP4Remuxer
+        },
+        {
+          demux: MP3Demuxer,
+          remux: MP4Remuxer
+        }
       ];
 
       // probe for content type
@@ -90,7 +113,12 @@ class DemuxerInline {
         }
       }
       if (!mux) {
-        observer.trigger(Event.ERROR, { type: ErrorTypes.MEDIA_ERROR, details: ErrorDetails.FRAG_PARSING_ERROR, fatal: true, reason: 'no demux matching with content found' });
+        observer.trigger(Event.ERROR, {
+          type: ErrorTypes.MEDIA_ERROR,
+          details: ErrorDetails.FRAG_PARSING_ERROR,
+          fatal: true,
+          reason: 'no demux matching with content found'
+        });
         return;
       }
       // so let's check that current remuxer and demuxer are still valid
